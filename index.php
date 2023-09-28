d4fd99b5751df2ea85cac349badeb270ba4a4291
Julia <jsgoransson@gmail.com>
2023-09-28 21:22:37 +0200

    Added h1, nr of teams and table
    Added HTML-tags for h1 and the table that is going to contain the teams data. Also connected index page to the teams.php and table.php pages that contains the php functions.

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php require __DIR__ . '/header.php'; ?>
+<?php require __DIR__ . '/header.php'; ?> <!-- Header -->
 
+<body>
+    <h1>Some teams</h1>
+    <p>There are <?php require __DIR__ . '/teams.php'; ?> teams. </p> <!-- Number of teams in the data -->
 
+    <table>
+        <tr>
+            <th>Team</th>
+            <th>League</th>
+            <th>Last time champions</th>
+            <th>City</th>
+            <th>Nickname</th>
+            <th>Website</th>
+        </tr>
+        <?php require __DIR__ . '/table.php'; ?> <!-- Start loop for data table-->
+    </table>
+</body>
+</html>
 
