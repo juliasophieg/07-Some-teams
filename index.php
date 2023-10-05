13d2f5a671ef6d663c08e5ae14340d3db2e124ab
Julia <jsgoransson@gmail.com>
2023-10-05 21:36:23 +0200

    Structure update

--- index.php
+++ index.php
@@ -1,24 +1,36 @@
-<?php require __DIR__ . '/header.php'; ?> <!-- Header -->
+<?php require __DIR__ . '/header.php'; ?> <!-- Fetching code for the header -->
 
 <body>
-    <h1>Some teams</h1>
-    <p>Here is a nice table with <?php require __DIR__ . '/teams.php'; ?> teams from <?php require __DIR__ . '/cities.php'; ?> </p> <!-- Number of teams in the data -->
-
-    <table>
-        <thead>
-            <tr>
-                <th>Team</th>
-                <th>League</th>
-                <th>Last time champions</th>
-                <th>City</th>
-                <th>Nickname</th>
-                <th>Website</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php require __DIR__ . '/table.php'; ?> <!-- Fetching data for the table-->
-        </tbody>
-    </table>
+    <h1 class="logo">Some teams</h1>
+    <main>
+        <div class="container">
+            <div class="intro">
+                <h2>Hi!</h2>
+                <p>Below you can find a <b>nice table</b> with information about <?php require __DIR__ . '/teams.php'; ?> football teams.</p><!-- Fetching data for team count-->
+            </div>
+            <div class="cities">
+                <h3>The team's homecities are:</h3>
+                <div class="city-buttons">
+                    <?php require __DIR__ . '/cities.php'; ?> <!-- Fetching data for the unique cities-->
+                </div> 
+            </div>
+        </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Team</th>
+                        <th>League</th>
+                        <th>Last time champions</th>
+                        <th>City</th>
+                        <th>Nickname</th>
+                        <th>Website</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php require __DIR__ . '/table.php'; ?> <!-- Fetching data for the table-->
+                </tbody>
+            </table>  
+    </main>
 </body>
 </html>
 
